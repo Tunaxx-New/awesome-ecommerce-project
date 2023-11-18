3139ffe9be3d0b5dea53e7fd7d4e21acc34e54ed
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2023-11-19 01:12:40 +0600

    Authorization Back 0.1
    Initial Commit
    
    Delete Authorization directory
    
    Authorization create
    
    Authorization create
    
    Initial commit
    
    Initial commit

---
+++ service/Authorization/service/auth/src/main/java/com/kn/auth/controllers/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.kn.auth.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kn.auth.requests.AuthenticationRequest;
+import com.kn.auth.responses.AuthenticationResponse;
+import com.kn.auth.services.AutheticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AutheticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request) {
+        return service.register(request);
+    }
+    
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request) {
+        return service.authenticate(request);
+    }
+
+}
