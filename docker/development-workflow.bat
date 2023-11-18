ea9cd65178d90c296385f217ec035dad765f7643
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2023-11-19 03:38:34 +0600

    dev workflow

--- docker/development-workflow.bat
+++ docker/development-workflow.bat
@@ -1,6 +1,6 @@
 echo "Starting project on development mode"
-docker-compose -f Authorization/docker-compose-dev.yml -p ecommerce-greenshop-dev up --build
+docker-compose -f docker/Authorization/docker-compose-dev.yml -p ecommerce-greenshop-dev up --build
 echo "Show logs"
-docker-compose -f Authorization/docker-compose-dev.yml logs
+docker-compose -f docker/Authorization/docker-compose-dev.yml logs
 echo "Docker compose down"
-docker-compose -f Authorization/docker-compose-dev.yml down
+docker-compose -f docker/Authorization/docker-compose-dev.yml down
