f8c6db61a9731e71bb464fd4d6ab65a74fbd46c6
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2023-11-19 21:02:49 +0600

    fix: development.bat more complex with jenkins

--- docker/development.bat
+++ docker/development.bat
@@ -1,2 +1,11 @@
 echo "Starting project on development mode"
-docker-compose -f ./Authorization/docker-compose-dev.yml -p greenshop-dev up --build
+docker-compose -f ./Authorization/docker-compose-dev.yml -p greenshop-dev up --build > docker_log.txt 2>&1
+
+REM Wait until the specified word appears in the logs
+:waitLoop
+docker-compose logs | findstr "Tomcat started on port(s): 8080" > nul
+if %errorlevel% neq 0 (
+    timeout /nobreak /t 1 > nul
+    goto waitLoop
+)
+exit 0
