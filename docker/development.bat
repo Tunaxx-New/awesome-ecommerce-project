b9dfc06952bcb007204e95a5963cc5acea907280
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2024-05-28 19:34:59 +0500

    update: docker compose in detached mode

--- docker/development.bat
+++ docker/development.bat
@@ -1,2 +1,2 @@
 echo "Starting project on development mode"
-docker-compose -f ./Authorization/docker-compose-dev.yml -p greenshop-dev up --build --force-recreate
+docker-compose -f ./Authorization/docker-compose-dev.yml -p greenshop-dev up -d --build --force-recreate
